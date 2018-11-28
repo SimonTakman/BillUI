705f924c647abe7ce5e8ed5791272570bc50cdbc
SimonTakman <simontakman@gmail.com>
2018-11-28 13:45:58 +0100

    Updated Main to generate 8 new layers depending on the selected component

--- src/main.js
+++ src/main.js
@@ -2,20 +2,30 @@ import sketch from 'sketch'
 import {mutateColor} from './colorUtil'
 // documentation: https://developer.sketchapp.com/reference/api/
 
+function duplicateNewLayers(obj, numberOfLayers){
+  for(let i = 0; i < numberOfLayers; i++){
+    let tmpObj = obj.duplicate()
+    tmpObj.frame.y = tmpObj.frame.y + (i+1)*tmpObj.frame.height*2
+    tmpObj.name = tmpObj.name + "." + i
+    let color = mutateColor(tmpObj.style.fills[0].color)
+    tmpObj.style.fills[0].color = color
+    let tmpNativeObj = tmpObj.sketchObject
+    tmpNativeObj.setCornerRadiusFloat(Math.floor(Math.random() * 30))
+  }
+}
+
 //This is our main function that triggers when we start the file
 export default function() {
-  //let hexNumber = rgbToHex(122,32,11)
   let document = sketch.getSelectedDocument()
-  //let page = document.selectedPage
-  //let page = document.selectedPage
-  //let layer = document.selectedLayers
-  let shape = document.getLayersNamed("Rectangle")[0]
-  console.log(shape)
-  let sketchObject = shape.sketchObject
-  console.log(sketchObject)
-  let color = mutateColor(shape.style.fills[0].color)
-  shape.style.fills[0].color = color
+  let selectedLayers = document.selectedLayers
+  if(!selectedLayers.isEmpty){
+    let shape = selectedLayers.layers[0]
+    duplicateNewLayers(shape,8)
+  }
+  //console.log(selectedLayers.layers)
+  //let shape = document.getLayersNamed("Rectangle")[0]
+  //createEightNewLayers(shape)
+  //console.log(sketchObject)
   // Move this function to shape
-  sketchObject.setCornerRadiusFloat(Math.floor(Math.random() * 30))
   sketch.UI.message("It's wow  🙌")
 }
